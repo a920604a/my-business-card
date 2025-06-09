99e46537182a1116323e1038a85fe1f3b1db7c45
a920604a <a920604a@gmail.com>
2025-06-09 16:58:39 +0800

    fix: update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-business-card/',  // 👈 根據你 repo 名稱設定
   plugins: [react()],
-})
+});
